b8a6983f129a534dce1e8da8af14d4363b26fc13
unknown <ubkhf8@gmail.com>
2021-07-03 11:37:44 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,47 @@
+
+const mongoose = require('mongoose');
+const express = require('express');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+   
+    zip :{
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: 'Srinagar'
+    },
+
+});
+
+userSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+userSchema.set('toJSON', {
+    virtuals: true,
+});
+
+exports.User = mongoose.model('User', userSchema);
+exports.userSchema = userSchema;
